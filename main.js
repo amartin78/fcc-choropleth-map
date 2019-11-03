7f8ba12a3fcd60ff98a3680687f9c22e038bab38
antonio <ammj24@hotmail.com>
2019-11-03 19:40:49 +0000

    add map with counties

--- main.js
+++ main.js
@@ -12,10 +12,11 @@ d3.json('https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/
 
 
 
-       
 function cMap(dataset) {
 
     console.log(dataset['bbox'])
+
+    const us = dataset;
 
     d3.select('body')
         .append('h1')
@@ -32,11 +33,33 @@ function cMap(dataset) {
     const height = 500
     const padding = 80
 
+    const color = d3.scaleQuantize([1, 10], d3.schemeBlues[9])
+
     const svg = d3.select('body')
                     .append('svg')
                     // .attr('width', width)
                     // .attr('height', height)
                     .attr('viewBox', dataset['bbox'])
+
+    const path = d3.geoPath();
+
+    svg.append('g')
+       .selectAll('path')
+       .data(topojson.feature(us, us.objects.counties).features)
+       .join('path')
+    //    .attr('fill', d => color(dataset.get(d.id)))
+       .attr('class', 'county')
+       .attr('d', path)
+
+    svg.append('path')
+       .datum(topojson.mesh(us, us.objects.states, (a, b) => a !== b))
+       .attr('fill', 'none')
+       .attr('stroke', 'white')
+       .attr('stroke-linejoin', 'round')
+       .attr('d', path)
+
+    return svg.node();
+
 
     
                     
